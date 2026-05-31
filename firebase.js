81f8516707ac8a76c4125e07698828c02e5e799d
fast-alsari <ahmedalsulta@icloud.com>
2026-06-01 00:12:24 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,30 @@
+// firebase.js
 
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import {
+getDatabase
+}
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
+
+const firebaseConfig = {
+
+apiKey:"YOUR_API_KEY",
+
+authDomain:"YOUR_PROJECT.firebaseapp.com",
+
+projectId:"YOUR_PROJECT",
+
+databaseURL:
+"https://YOUR_PROJECT-default-rtdb.firebaseio.com"
+
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+const db =
+getDatabase(app);
+
+export { db };
