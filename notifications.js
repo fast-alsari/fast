e1759efb923c7ff8f7314c11aa95ab090b2057b7
fast-alsari <ahmedalsulta@icloud.com>
2026-06-05 21:29:39 +0300

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC7QzQTYMSGRV_nraJBneBqDqYGhugF0Pw",
+  authDomain: "fast-937c9.firebaseapp.com",
+  databaseURL: "https://fast-937c9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fast-937c9",
+  storageBucket: "fast-937c9.firebasestorage.app",
+  messagingSenderId: "221388561840",
+  appId: "1:221388561840:web:ec9d75183ddd7d44be0a75"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+console.log("FCM Started");
